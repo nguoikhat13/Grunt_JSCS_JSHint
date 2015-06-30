11297906cf0fe802eeefb9df7091eba57f583539
nguoikhat13 <nguoikhat13@gmail.com>
2015-06-30 15:32:20 -0400

    Installation of all packages for grunt to run jscs and jshint.

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // ---------------------------------------------------------------------- //
+    jscs: {
+      src: [
+		'*.js'
+      ],
+      options: {
+        config: 'google.json',
+        fix: true,
+        reporter: 'node_modules/jscs-html-reporter/jscs-html-reporter.js',
+        reporterOutput: 'jscs-report.html'
+      }
+    },
+    // ---------------------------------------------------------------------- //
+    jshint: {
+      options: {
+        node: true,
+        reporter: require('jshint-html-reporter'),
+        reporterOutput: 'jshint-report.html'
+      },
+      files: {
+        src: [
+		'*.js'
+        ]
+      }
+    }
+  });
+
+  //  require('logfile-grunt')(grunt, { keepColors: true });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+  // Default task(s)
+  grunt.task.registerTask('jscsLog', function() {
+    require('logfile-grunt')(grunt, {
+      filePath: './logs/jscsTest.txt',
+      clearLogFile: true
+    });
+  });
+  grunt.task.registerTask('jshintLog', function() {
+    require('logfile-grunt')(grunt, {
+      filePath: './logs/jshintTest.txt',
+      clearLogFile: true
+    });
+  });
+  grunt.registerTask('default', [
+    'jscs',
+    'jshint'
+  ]);
+  grunt.registerTask('jscsTask', [
+    'jscsLog',
+    'jscs'
+  ]);
+  grunt.registerTask('jshintTask', [
+    'jshintLog',
+    'jshint'
+  ]);
+};
